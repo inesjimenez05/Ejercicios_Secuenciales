6fab80e18d55a7e0efaa88eced419569c90bb861
Inés <al_ijimenezg@iessierradelasvillas.org>
2022-10-20 10:15:58 +0200

    Jueves_20.10

--- ejercicio3f.py
+++ ejercicio3f.py
@@ -1,1 +1,8 @@
 #3_Dados los catetos de un triángulo rectángulo, calcular su hipotenusa
+
+def calcularhipotenusa():
+    cateto1= int(input("Cateto 1: \n"))
+    cateto2= int(input("Cateto 2: \n"))
+    print("La hipotenusa del triángulo es:", (((cateto1**2)+(cateto2**2))**(1/2)))
+
+calcularhipotenusa()
---
+++ ejercicio4f.py
@@ -0,0 +1,9 @@
+#4_Dados dos números, mostrar la suma, resta, división y multiplicación de ambos
+
+def imprimirSuma ():
+    num1=int(input("Di un numero: \n"))
+    num2=int(input("Di un numero: \n"))
+    suma= num1+num2
+    return suma
+
+    
